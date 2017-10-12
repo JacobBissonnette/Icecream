f464ce309e6a9e985b736444d51b2161ffed4ed4
Its ya boi jake <31927321+JacobBissonnette@users.noreply.github.com>
2017-10-12 09:42:45 -0400

    Add files via upload

---
+++ ice-cream.js
@@ -0,0 +1,10 @@
+flavor = prompt("ya neopalitan icecream, whatchu want ");
+
+if(flavor == "vanilla" || flavor == "choclate"){
+	alert("here your cream "+flavor+".");
+}
+
+
+else if(flavor=="strawberry"){
+ alert(nah strawberry machine broke but we got vanilla and chocolate tho);
+}
---
+++ icecream.js
@@ -0,0 +1,9 @@
+var flavor=prompt("what flavor would you like ");
+
+if(flavor == "chocolate" ||flavor == "vanilla"){
+alert("here's youre "+flavor+".");	
+}
+else if(flavor=="Strawberry"){
+	
+}
+alert("kys");
